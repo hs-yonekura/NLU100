5f04f3f9fea21170844264963003c583942dfd8e
hs-yonekura <y-yonekura@science.co.jp>
2019-12-12 21:37:43 +0900

    1-3 fixed

--- chapter01/knock03.py
+++ chapter01/knock03.py
@@ -1,4 +1,4 @@
 # codeing: utf-8
 import re
-print([len(w) for w in re.sub(r'[\.,]', '', 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.').split(' ')
+print([len(w) for w in re.split(r'[\., ]', 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.')
 ])
