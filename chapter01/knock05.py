1e8a334fd888252511deed5b7a8ac393aea62b54
hs-yonekura <y-yonekura@science.co.jp>
2019-12-14 23:11:27 +0900

    test

--- chapter01/knock05.py
+++ chapter01/knock05.py
@@ -1,4 +1,5 @@
 def make_ngram(arr: [], n: int):
     return list(tuple(arr[i:i + n]) for i in range(len(arr) + 1 - n))
 
+print(make_ngram('I am an NLPer'.split(' '), 2))
 print(make_ngram('I am an NLPer', 2))
